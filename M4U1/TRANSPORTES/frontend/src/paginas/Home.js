9c38e4f12fc9f1124f17357c05617949273a6e76
Maria Paula Vera <mariapaulavera@hotmail.com>
2023-06-15 15:47:30 -0300

    15/06

--- M4U1/TRANSPORTES/frontend/src/paginas/Home.js
+++ M4U1/TRANSPORTES/frontend/src/paginas/Home.js
@@ -8,21 +8,21 @@ const Home = (props)=> {
                 <img className="foto_inicio" src="images/home/home.jpg"  alt="Carga"/>
             </div>
             <div className="columnas">
-                <div className="columna">
+                <div className="columna uno">
                     <h2>Personas </h2>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore a accusantium harum suscipit quae,
                         natus dicta. Quam veritatis nemo esse? Labore inventore excepturi eius, voluptate nobis enim a
                         dolores natus.
                     </p>
                 </div>
-                    <div className="columna">
+                    <div className="columna dos">
                     <h2>PYMES </h2>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt saepe optio dignissimos, temporibus
                         sed rem labore esse eveniet nam quo natus at harum culpa reprehenderit consequatur ratione? Nostrum,
                         exercitationem expedita!
                     </p>
                 </div>
-                <div className="columna">
+                <div className="columna tres">
                     <h2>Corporativos </h2>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt saepe optio dignissimos, temporibus
                         sed rem labore esse eveniet nam quo natus at harum culpa reprehenderit consequatur ratione? Nostrum,
