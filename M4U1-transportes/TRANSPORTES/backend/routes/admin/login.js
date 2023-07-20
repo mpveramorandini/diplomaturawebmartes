2778ab849fcf4faf4ceca273e12923b50485b95f
Maria Paula Vera <mariapaulavera@hotmail.com>
2023-07-20 10:25:33 -0300

    ok

---
+++ M4U1-transportes/TRANSPORTES/backend/routes/admin/login.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel= require('./../../models/usuariosModel');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin/login',{  //login.hbs
+    layout:'admin/layout' //layout.hbs
+  });
+});
+
+router.get('/logout', function(req, res, next){
+  req.session.destroy(); //destruir las variables de sesion (id y usuario)
+  res.render('admin/login', {
+    layout: 'admin/layout'
+  });
+}); //cierro logout
+
+router.post('/', async(req, res, next) =>{
+  try{
+    var usuario = req.body.usuario; //capturando info//
+    var password = req.body.password;
+
+    var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+
+    if (data != undefined ){
+      req.session.id_usuario = data.id;
+      req.session.nombre = data.usuario; 
+
+      res.redirect('/admin/novedades');
+    } else{
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+  } catch (error){
+    console.log(error);
+  }
+})
+
+
+module.exports = router;
--- M4U1-transportes/TRANSPORTES/backend/routes/admin/novedades.js
+++ M4U1-transportes/TRANSPORTES/backend/routes/admin/novedades.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin/login',{  //login.hbs
-    layout:'admin/layout' //layout.hbs
+  res.render('admin/novedades',{ 
+    layout:'admin/layout', 
+    persona: req.session.nombre
   });
 });
 
