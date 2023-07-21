23cfb61959c29819222e095f3254873b92a6d9f4
Maria Paula Vera <mariapaulavera@hotmail.com>
2023-07-21 10:50:48 -0300

    ok

--- M4U1-transportes/TRANSPORTES/backend/routes/admin/login.js
+++ M4U1-transportes/TRANSPORTES/backend/routes/admin/login.js
@@ -26,7 +26,6 @@ router.post('/', async(req, res, next) =>{
     if (data != undefined ){
       req.session.id_usuario = data.id;
       req.session.nombre = data.usuario; 
-
       res.redirect('/admin/novedades');
     } else{
       res.render('admin/login', {
